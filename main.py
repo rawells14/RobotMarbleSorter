1d933474c7ef0140f0ce0df2a30a0a3205826930
Ryan Wells <rawells14@gmail.com>
2017-02-22 09:37:51 -0600

    added path

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+path = '/sys/class/tacho-motor/motor';
+
+
 def move_motor(speed, motor_number, time):
-    file = open('/sys/class/tacho-motor/motor' + motor_number + '/speed_sp', 'w')
+    file = open(path + motor_number + '/speed_sp', 'w')
     file.write(str(speed))
     file.close()
 
-    file = open('/sys/class/tacho-motor/motor' + motor_number + '/time_sp', 'w')
+    file = open(path + motor_number + '/time_sp', 'w')
     file.write(str(time))
     file.close()
 
-    file = open('/sys/class/tacho-motor/motor' + motor_number + '/command', 'w')
+    file = open(path + motor_number + '/command', 'w')
     file.write('run-timed')
     file.close()
 
 
 def stop(motor_number):
-    file = open('/sys/class/tacho-motor/motor' + motor_number + '/command', 'w')
+    file = open(path + motor_number + '/command', 'w')
     file.write('')
     file.close()
 
 
-move_motor(500)
+move_motor(500, 2, 1000)
