66a99a2e77936efda5cfa769ffb354ca51879058
Ryan Wells <rawells14@gmail.com>
2017-02-22 09:44:29 -0600

    made motor number a string

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ path = '/sys/class/tacho-motor/motor';
 
 
 def move_motor(speed, motor_number, time):
-    file = open(str(path + motor_number + '/speed_sp'), 'w')
+    motor_number = str(motor_number)
+    file = open(path + motor_number + '/speed_sp', 'w')
     file.write(str(speed))
     file.close()
 
-    file = open(str(path + motor_number + '/time_sp'), 'w')
+    file = open(path + motor_number + '/time_sp', 'w')
     file.write(str(time))
     file.close()
 
-    file = open(str(path + motor_number + '/command'), 'w')
+    file = open(path + motor_number + '/command', 'w')
     file.write('run-timed')
     file.close()
 
 
 def stop(motor_number):
-    file = open(str(path + motor_number + '/command'), 'w')
+    motor_number = str(motor_number)
+    file = open(path + motor_number + '/command', 'w')
     file.write('')
     file.close()
 
-
-move_motor(500, 2, 1000)
+    move_motor(500, 2, 1000)
