6d2aa5f16da8c80fd0a0bebbcfcaa6882a78b87b
Ryan Wells <rawells14@gmail.com>
2017-02-23 16:57:27 -0600

    Separated motor from controlling motor

---
+++ motor.py
@@ -0,0 +1,23 @@
+path = '/sys/class/tacho-motor/motor'
+
+
+def move_motor(speed, motor_number, time):
+    motor_number = str(motor_number)
+    file = open(path + motor_number + '/speed_sp', 'w')
+    file.write(str(speed))
+    file.close()
+
+    file = open(path + motor_number + '/time_sp', 'w')
+    file.write(str(time))
+    file.close()
+
+    file = open(path + motor_number + '/command', 'w')
+    file.write('run-timed')
+    file.close()
+
+
+def stop(motor_number):
+    motor_number = str(motor_number)
+    file = open(path + motor_number + '/command', 'w')
+    file.write('')
+    file.close()
