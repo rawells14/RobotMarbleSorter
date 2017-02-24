3711b47979fac4334bac96be4c6720b7144a8cbc
Ryan Wells <rawells14@gmail.com>
2017-02-23 22:35:59 -0600

    Put the motor back into 1 file

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-from motor import *
+motor_path = '/sys/class/tacho-motor/motor'
+
+
+def move_motor(speed, motor_number, time):
+    motor_number = str(motor_number)
+    file = open(motor_path+ motor_number + '/speed_sp', 'w')
+    file.write(str(speed))
+    file.close()
+
+    file = open(motor_path + motor_number + '/time_sp', 'w')
+    file.write(str(time))
+    file.close()
+
+    file = open(motor_path + motor_number + '/command', 'w')
+    file.write('run-timed')
+    file.close()
+
+
+def stop(motor_number):
+    motor_number = str(motor_number)
+    file = open(motor_path + motor_number + '/command', 'w')
+    file.write('')
+    file.close()
 
 move_motor(500, 1, 1000)
 move_motor(500, 2, 1000)
