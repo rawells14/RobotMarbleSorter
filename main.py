1a01d785c669e9f5a5134240a958e84eacf7e33d
Ryan Wells <rawells14@gmail.com>
2017-02-23 21:40:14 -0600

    both motors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from motor import *
 
-move_motor(500, 3, 1000)
+move_motor(500, 1, 1000)
+move_motor(500, 2, 1000)
--- motor.py
+++ motor.py
@@ -1,17 +1,17 @@
-path = '/sys/class/tacho-motor/motor'
+motor_path = '/sys/class/tacho-motor/motor'
 
 
 def move_motor(speed, motor_number, time):
     motor_number = str(motor_number)
-    file = open(path + motor_number + '/speed_sp', 'w')
+    file = open(motor_path+ motor_number + '/speed_sp', 'w')
     file.write(str(speed))
     file.close()
 
-    file = open(path + motor_number + '/time_sp', 'w')
+    file = open(motor_path + motor_number + '/time_sp', 'w')
     file.write(str(time))
     file.close()
 
-    file = open(path + motor_number + '/command', 'w')
+    file = open(motor_path + motor_number + '/command', 'w')
     file.write('run-timed')
     file.close()
 
