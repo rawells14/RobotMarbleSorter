a6ebe812a3159f0c78bd928366b74360e6afa8f8
gracewes <gracewes@tamu.edu>
2017-03-05 20:24:25 -0600

    Okay so this almost works.
    Added another function to run motor continuously until scan is finished
    and splice out odd indexes of barcode because they are unnecessary. Also
    changes readings of the number 6 to zeros because the internet said this
    was the color sensor value for white and idk why it just now started
    spitting out 6es, but its weird so I switched them to zeros.

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ import time
 motor_path = '/sys/class/tacho-motor/motor'
 barcode_sensor_path = '/sys/class/lego-sensor/sensor1'
 
-def move_motor(speed, motor_number, time):
+def move_motor(speed, motor_number):
+    motor_number = str(motor_number)
+    file = open(motor_path + motor_number + '/speed_sp', 'w')
+    file.write(str(speed))
+    file.close()
+    
+    file = open(motor_path + motor_number + '/command', 'w')
+    file.write('run-forever')
+    file.close()
+
+def move_motor_timed(speed, motor_number, time):
     motor_number = str(motor_number)
     file = open(motor_path+ motor_number + '/speed_sp', 'w')
     file.write(str(speed))
@@ -23,7 +33,7 @@ def move_motor(speed, motor_number, time):
 def stop(motor_number):
     motor_number = str(motor_number)
     file = open(motor_path + motor_number + '/command', 'w')
-    file.write('')
+    file.write('stop')
     file.close()
 
 def poll_barcode_sensor():
@@ -39,14 +49,41 @@ def poll_barcode_sensor():
 def read_barcode():
 
     # 20 total binary values
-    move_motor(100, 1, 9000)
+    move_motor_speed(200, 1, 5000)
     barcode = []
     for i in range(0, 20):
-        time.sleep(.2)
+        time.sleep(.12)
         val = poll_barcode_sensor()
         print(val)
         barcode.append(int(val))
     return barcode
+
+
+def read_barcode_auto():
+
+	#wait until see a white line then switches to black line to start polling
+	move_motor(220, 1)
+	barcode = []
+	val = poll_barcode_sensor()
+	while int(val)==1:
+		val = poll_barcode_sensor()
+	while int(val)==0:
+		val = poll_barcode_sensor()
+	print(val)
+	for i in range(0,17):
+		time.sleep(.115)
+		val = poll_barcode_sensor()
+		print(val)
+		#sometimes white is read as its color value 6 so I am changing it to 0
+		if int( val ) == 6:
+			val = 0
+		barcode.append(int(val))
+	stop(1)
+	#get rid of odd indexes to get barcode without spaces
+	print(barcode)
+	for i in range (0,9):
+		del barcode[i]
+	return barcode
 
 # moves the dispenser piston
 # move_motor(500, 0, 1000)
@@ -54,5 +91,15 @@ def read_barcode():
 # moves the barcode motor
 #   move_motor(500, 1, 1000)
 
-print(read_barcode())
-
+code = (read_barcode_auto())
+colors = ['White Glass', 'Red Glass', 'Blue Glass', 'Steel/HDPE']
+type = ['Large', 'Small']
+color_code = []
+type_code = []
+for i in [2,1,0]:
+	color_code.append(code[i])
+for i in [7,6,5,4,3]:
+	type_code.append(code[i])
+print(color_code)
+print('\n')
+print(type_code)
