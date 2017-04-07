5ae5f0ae0d581c27385443c538a25251397367a5
Ryan Wells <rawells14@gmail.com>
2017-04-07 10:02:08 -0500

    Servo motor popping marbles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import time
 from Motor import Motor
-from Sensor import Sensor
-motor = Motor(0)
+motor = Motor(2)
 
-motor.move_motor_timed(1000, 1000)
+motor.move_motor_abs_pos(-75, 1000)
+time.sleep(2)
+motor.move_motor_abs_pos(0, 1000)
