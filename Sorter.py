545f37dd47760ea1eb1d8032eb6100fa81fa5235
Ryan Wells <rawells14@gmail.com>
2017-04-11 20:12:55 -0500

    Sorter Class should be almost complete

--- Sorter.py
+++ Sorter.py
@@ -1,11 +1,24 @@
 class Sorter(object):
         positions = []
-
+        colors = []
         def __init__(self, conveyer_number, popper_number):
             self.conveyer_motor = Motor(conveyer_number)
             self.popper_motor = Motor(popper_number)
 
         def pop():
-            motor.move_motor_abs_pos(-75, 1000)
-            time.sleep(2)
-            motor.move_motor_abs_pos(0, 1000)
+            self.popper_motor.move_motor_abs_pos(-75, 1000)
+            time.sleep(1)
+            self.popper_motor.move_motor_abs_pos(0, 1000)
+
+
+        def move_to_marble(color):
+            for i in range(len(colors)):
+                if colors[i] == color:
+                    self.conveyer_motor.move_motor_abs_pos(positions[i], 100)
+                    return
+            print('Couldn\'t find color)
+
+        def pop_marble(color):
+            move_to_marble(color)
+            time.sleep(5)
+            pop()
--- main.py
+++ main.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python
+import time
 from Motor import Motor
-from Barcode import Barcode
-from PelletIdentifier import PelletIdentifier
-from Color import Color
-
-i = 0;
-totalMarbles = [0,0,0,0,0,0,0,0]
-dosage = [0,0,0,0,0,0,0,0]
-#read barcodes and set dosage
-b = Barcode(0)
-for (i < 4):
-    newdosage = b.read_barcode() #change barcode functions to return dosage needed in the same form as
-                    #dosage so that we can just add it to the total.
-    #dosage = dosage + newdosage #may need for loop to add.
-    i += 1
-
-#identify and sort
-p = PelletIdentifier(1)
-while(#needs to be written: p.hasMarble()):
-    color = Color(p.poll_color())
-    #change function to return a number instead of a string and keep all arrays with colors in the same order.
-    totalMarbles[color.identify_color()] += 1;
+motor = Motor(2)
